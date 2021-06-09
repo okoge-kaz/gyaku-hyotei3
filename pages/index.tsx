dd6f8f001cabc0cc16d8a91091ca3112dc6dad24
kazuki <kaz.tokyo.tech20@gmail.com>
2021-06-09 11:49:10 +0900

    miner change

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { Container } from 'react-bootstrap'
 import SubHead from './components/SubHeader'
+import Content from './Content'
+import { Segment } from '../interfaces/segment'
 
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1> Hello Next.js ++</h1>
+    <h1> Hello Next.js ++++--</h1>
   </Layout>
 )
 
