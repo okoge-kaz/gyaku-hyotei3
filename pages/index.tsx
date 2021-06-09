bf267ed5efa9af87c5c1876d43d9ca03d067f7f2
kazuki <kaz.tokyo.tech20@gmail.com>
2021-06-09 11:47:39 +0900

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
 
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1> Hello Next.js +</h1>
+    <h1> Hello Next.js ++++</h1>
   </Layout>
 )
 
