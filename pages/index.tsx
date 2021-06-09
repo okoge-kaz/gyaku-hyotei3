68c6e59e8213a1ac9c97fad84b1b56ddfd3e8003
kazuki <kaz.tokyo.tech20@gmail.com>
2021-06-09 11:48:26 +0900

    miner edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { Container } from 'react-bootstrap'
+import SubHead from './components/SubHeader'
 
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1> Hello Next.js ++++</h1>
+    <h1> Hello Next.js ++</h1>
   </Layout>
 )
 
