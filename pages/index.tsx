68732bfd94d49547b18712ff6ebb98798ec174f8
kazuki <kaz.tokyo.tech20@gmail.com>
2021-06-09 11:45:09 +0900

    adding index.module.scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1> Hello Next.js </h1>
+    <h1> Hello Next.js ++</h1>
   </Layout>
 )
 
