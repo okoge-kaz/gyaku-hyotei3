501c1005d5ad5e8e6b6f95e2ad40202feb927383
kazuki <kaz.tokyo.tech20@gmail.com>
2021-07-25 21:43:42 +0900

    文字変更

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -5,7 +5,7 @@ import { Jumbotron, Container } from 'react-bootstrap'
 const SubHeader: FC = () => (
   <Container className={styles.ContainerPadding}>
     <Jumbotron className={styles.JumbotronSize}>
-      <h1>Gyaku - Hyotei</h1>
+      <h1>逆評定</h1>
       <p>
         説明のてきすと説明のてきすと説明のてきすと説明のてきすと説明のてきすと説明のてきすと説明のてきすと
       </p>
